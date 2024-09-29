bbcd1f09fb431e07ee504020a181c6d7d134a21c
Numero3bis <numero3bis@yahoo.fr>
2024-09-29 19:00:44 +0200

    [ADD] update user model

--- lib/models/user.model.dart
+++ lib/models/user.model.dart
@@ -1,15 +1,23 @@
 class User {
   final String firstName;
   final String lastName;
-  User({
-    required this.firstName,
-    required this.lastName,
-  });
+  final String city;
+  final String country;
+  final String smallPictureUrl;
+  User(
+      {required this.firstName,
+      required this.lastName,
+      required this.city,
+      required this.country,
+      required this.smallPictureUrl});
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
       firstName: map['name']['first'] ?? '',
       lastName: map['name']['last'] ?? '',
+      city: map['location']['city'] ?? '',
+      country: map['location']['country'] ?? '',
+      smallPictureUrl: map['picture']['thumbnail'] ?? '',
     );
   }
 }
