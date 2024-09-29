eae8e6a1f86bd9f10aec35bb6eb7610dd38e5402
Numero3bis <numero3bis@yahoo.fr>
2024-09-29 19:08:21 +0200

    [FIX] update userTile with new data

--- lib/components/user_tile.component.dart
+++ lib/components/user_tile.component.dart
@@ -15,16 +15,40 @@ class UserTileComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: (index % 2 == 1)
-                ? const Color.fromARGB(255, 239, 242, 243)
-                : Colors.white),
-        padding:
-            const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 10),
-        margin: const EdgeInsets.only(left: 5, bottom: 2.5, right: 5),
-        child: Row(
-          children: [Text(user.firstName), Text(" ${user.lastName}")],
-        ));
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: (index % 2 == 1)
+              ? const Color.fromARGB(255, 239, 242, 243)
+              : Colors.white),
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 10),
+      margin: const EdgeInsets.only(left: 5, bottom: 2.5, right: 5),
+      child: Row(children: [
+        Container(
+          margin: const EdgeInsets.only(right: 10),
+          height: 45,
+          width: 45,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage(user.smallPictureUrl)),
+            shape: BoxShape.circle,
+          ),
+        ),
+        Expanded(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(
+            children: [
+              Text(user.firstName),
+              if (user.lastName != '') Text(" ${user.lastName}")
+            ],
+          ),
+          Row(
+            children: [
+              Text(user.city),
+              if (user.country != '') Text(", ${user.country}"),
+            ],
+          )
+        ])),
+      ]),
+    );
   }
 }
